361ef77473476d8e2105a431e113cb83e027a298
davidpig <s9dazhuu@stud.uni-saarland.de>
2016-10-30 21:03:51 +0100

    passed the first part of lab1

--- lab/lock_server.cc
+++ lab/lock_server.cc
@@ -9,6 +9,7 @@
 lock_server::lock_server():
   nacquire (0)
 {
+	assert(pthread_mutex_init(&map_mutex, NULL) == 0);
 }
 
 lock_protocol::status
@@ -17,6 +18,34 @@ lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
   lock_protocol::status ret = lock_protocol::OK;
   printf("stat request from clt %d\n", clt);
   r = nacquire;
+  return ret;
+}
+
+lock_protocol::status
+lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &)
+{
+	lock_protocol::status ret = lock_protocol::OK;
+	//first critical section
+	assert(pthread_mutex_lock (&map_mutex) == 0);// only one thread can look into the mutex_map at a time
+	if(lock_map.find(lid) == lock_map.end()){ // server has never seen the lock id before
+		pthread_mutex_t new_mutex;				// add the new mutex in the map
+		assert(pthread_mutex_init(&new_mutex, NULL) == 0);
+		lock_map[lid] = new_mutex;
+		//assert(pthread_mutex_init(&lock_map[lid], 0) == 0);
+	}
+    assert(pthread_mutex_unlock (&map_mutex) == 0);
+  
+  	
+  	assert(pthread_mutex_lock (&lock_map[lid]) == 0); //thread tries to get the lock
+  	return ret;
+}
+
+lock_protocol::status
+lock_server::release(int clt, lock_protocol::lockid_t lid, int &)
+{
+  
+  lock_protocol::status ret = lock_protocol::OK;
+  assert(pthread_mutex_unlock (&lock_map[lid]) == 0);
   return ret;
 }
 
--- lab/lock_server.h
+++ lab/lock_server.h
@@ -5,19 +5,27 @@
 #define lock_server_h
 
 #include <string>
+#include <map>
+#include <pthread.h>
 #include "lock_protocol.h"
 #include "lock_client.h"
 #include "rpc.h"
+
 
 class lock_server {
 
  protected:
   int nacquire;
+  std::map<lock_protocol::lockid_t, pthread_mutex_t> lock_map; // each id can be associated with a mutex
+  pthread_mutex_t map_mutex; //a mutex for the mutex_map so that only one thread can read/write the data in the map in a
 
  public:
   lock_server();
   ~lock_server() {};
   lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
+
 };
 
 #endif 
