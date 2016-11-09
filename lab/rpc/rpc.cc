804e367d0f4fcb5429f13c7c68b64bba4f88e87e
Tony Hong <humanhong1001@gmail.com>
2016-11-09 12:06:23 +0100

    Extract return

--- lab/rpc/rpc.cc
+++ lab/rpc/rpc.cc
@@ -565,9 +565,9 @@ rpcs::add_reply(unsigned int clt_nonce, unsigned int xid,
 {
 	ScopedLock rwl(&reply_window_m_);
 
-	std::list<reply_t> list = reply_window_[clt_nonce];
+	std::list<reply_t>& reply_list = reply_window_[clt_nonce];
 	std::list<reply_t>::iterator it;
-	for (it  = list.begin(); it != list.end(); it++) {
+	for (it  = reply_list.begin(); it != reply_list.end(); it++) {
 		if (it->xid == xid){
 			it->cb_present = true;
 			// TODO: char copy
@@ -603,33 +603,47 @@ rpcs::checkduplicate_and_update(unsigned int clt_nonce, unsigned int xid,
 	ScopedLock rwl(&reply_window_m_);
 
 	std::list<reply_t>::iterator it;
+	rpcs::rpcstate_t state = NEW;
 
-	std::list<reply_t> list = reply_window_[clt_nonce];
-	for (it = list.begin(); it != list.end();) {
+	std::list<reply_t>& reply_list = reply_window_[clt_nonce];
+
+	for (it = reply_list.begin(); it != reply_list.end();) {
 		if (it->xid == xid){
-			if (!it->cb_present)
-				return INPROGRESS;
-			else {
-				b = &it->buf;
-				sz = &it->sz;
-				return DONE;
+			jsl_log(JSL_DBG_1, "reply_list.size(): %lu\n", reply_list.size());
+
+			if (it->xid <= xid_rep){
+				free(it->buf);
+				it = reply_list.erase(it);
+				state = FORGOTTEN;
+				// break;
+			} else if (!it->cb_present) {
+				state = INPROGRESS;
+				// break;
+			} else {
+				*b = it->buf;
+				*sz = it->sz;
+				state = DONE;
+				// break;
 			}
 		}
 		// smaller than the xid_rep, is FORGOTTEN
 		if (it->xid <= xid_rep){
 			free(it->buf);
-			it = list.erase(it);
-			return FORGOTTEN;
-		} else{
+			it = reply_list.erase(it);
+		} else { 
 			it++;
 		}
 	}
 
-	// not seen in the list, is NEW
-	struct rpcs::reply_t rt(xid);
-	std::list<reply_t> reply_t_list = reply_window_[clt_nonce];
-	reply_t_list.push_back(rt);
-	return NEW;
+	if (state != NEW) {
+		return state;
+	}
+	else {
+		// not seen in the list, is NEW
+		struct rpcs::reply_t rt(xid);
+		reply_window_[clt_nonce].push_back(rt);
+		return NEW;
+	}
 
 
 	// jsl_log(JSL_DBG_2, "rpcs::checkduplicate_and_update internal error: clt_nonce %u not found\n", clt_nonce);
