9655a33c82e19a37b2e5a657e76528b89b5bcb55
davidpig <s9dazhuu@stud.uni-saarland.de>
2017-01-01 20:29:06 +0100

    lab6 step one finished. i.e. test 4b 4c passed only by giving twice the same dicrectory

--- lab/extent_client.cc
+++ lab/extent_client.cc
@@ -27,16 +27,55 @@ extent_protocol::status
 extent_client::get(extent_protocol::extentid_t eid, std::string &buf)
 {
   extent_protocol::status ret = extent_protocol::OK;
-  ret = cl->call(extent_protocol::get, eid, buf);
+  if(extent_cache_map.count(eid) > 0){
+    buf = extent_cache_map[eid];
+  }else{
+    ret = cl->call(extent_protocol::get, eid, buf);
+    extent_cache_map[eid] = buf;
+  }
   return ret;
 }
+
+
+extent_protocol::status
+extent_client::put(extent_protocol::extentid_t eid, std::string buf)
+{
+  extent_protocol::status ret = extent_protocol::OK;
+  //int r;
+  //ret = cl->call(extent_protocol::put, eid, buf, r);
+  extent_cache_map[eid] = buf;
+  extent_protocol::attr a;
+  a.size = buf.size();
+  a.atime = a.ctime = a.mtime = time(NULL);
+  attr_cache_map[eid] = a;
+  return ret;
+}
+
+extent_protocol::status
+extent_client::remove(extent_protocol::extentid_t eid)
+{
+  extent_protocol::status ret = extent_protocol::OK;
+  //int r;
+  //ret = cl->call(extent_protocol::remove, eid, r);
+  extent_cache_map.erase(eid);
+  return ret;
+}
+
+
 
 extent_protocol::status
 extent_client::getattr(extent_protocol::extentid_t eid, 
 		       extent_protocol::attr &attr)
 {
   extent_protocol::status ret = extent_protocol::OK;
-  ret = cl->call(extent_protocol::getattr, eid, attr);
+
+  if(attr_cache_map.count(eid) > 0){
+    attr = attr_cache_map[eid];
+  }else{
+    ret = cl->call(extent_protocol::getattr, eid, attr);
+    attr_cache_map[eid] = attr;
+  }
+
   return ret;
 }
 
@@ -45,28 +84,34 @@ extent_client::setattr(extent_protocol::extentid_t eid,
           extent_protocol::attr a)
 {
   extent_protocol::status ret = extent_protocol::OK;
-  int r;
-  ret = cl->call(extent_protocol::setattr, eid, a, r);
+  std::string buf;
+  //int r;
+  //ret = cl->call(extent_protocol::setattr, eid, a, r);
+  assert(attr_cache_map.count(eid) > 0);
+
+  extent_protocol::attr old_a = attr_cache_map[eid];
+
+  if(a.size != old_a.size){
+    get(eid, buf);
+  }
+
+  if (a.size < old_a.size){  
+        buf = buf.substr(0, a.size);
+        extent_cache_map[eid] = buf;
+        assert(buf.size() == a.size);   
+    } else if (old_a.size < a.size){
+        buf.resize(a.size);
+        extent_cache_map[eid] = buf;
+  }
+
+  attr_cache_map[eid].size = a.size;
+  attr_cache_map[eid].atime = attr_cache_map[eid].mtime = attr_cache_map[eid].ctime = time(NULL);
+
+
   return ret;
 } 
 
 
-extent_protocol::status
-extent_client::put(extent_protocol::extentid_t eid, std::string buf)
-{
-  extent_protocol::status ret = extent_protocol::OK;
-  int r;
-  ret = cl->call(extent_protocol::put, eid, buf, r);
-  return ret;
-}
 
-extent_protocol::status
-extent_client::remove(extent_protocol::extentid_t eid)
-{
-  extent_protocol::status ret = extent_protocol::OK;
-  int r;
-  ret = cl->call(extent_protocol::remove, eid, r);
-  return ret;
-}
 
 
--- lab/extent_client.h
+++ lab/extent_client.h
@@ -6,10 +6,15 @@
 #include <string>
 #include "extent_protocol.h"
 #include "rpc.h"
+#include <map>
 
 class extent_client {
  private:
   rpcc *cl;
+
+  //extent map
+  std::map<extent_protocol::extentid_t, std::string> extent_cache_map;
+  std::map<extent_protocol::extentid_t, extent_protocol::attr> attr_cache_map;
 
  public:
   extent_client(std::string dst);
@@ -17,12 +22,14 @@ class extent_client {
   
   extent_protocol::status get(extent_protocol::extentid_t eid, 
 			      std::string &buf);
+  extent_protocol::status put(extent_protocol::extentid_t eid, std::string buf);
+  extent_protocol::status remove(extent_protocol::extentid_t eid);
+
   extent_protocol::status getattr(extent_protocol::extentid_t eid, 
 				  extent_protocol::attr &a);
   extent_protocol::status setattr(extent_protocol::extentid_t eid, 
 				  extent_protocol::attr a);
-  extent_protocol::status put(extent_protocol::extentid_t eid, std::string buf);
-  extent_protocol::status remove(extent_protocol::extentid_t eid);
+  
 };
 
 #endif 
