2693dbcb6d553d1f81eb1ffb235d78fa570397d5
Tony Hong <humanhong1001@gmail.com>
2017-01-20 15:23:28 +0100

    Fix: set srand()

--- lab/yfs_client.cc
+++ lab/yfs_client.cc
@@ -14,6 +14,7 @@
 
 yfs_client::yfs_client(std::string extent_dst, std::string lock_dst)
 {
+  srand(time(NULL) ^ getpid());
   ec = new extent_client(extent_dst);
   //lc = new lock_client(lock_dst);
   lc = new lock_client_cache(lock_dst);
