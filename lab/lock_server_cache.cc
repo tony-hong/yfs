bb52c11441ac1b8f05d2ca6a922ca3e9cf003f62
davidpig <s9dazhuu@stud.uni-saarland.de>
2017-02-11 12:54:24 +0100

    small fix to make the files makeable

--- lab/lock_server_cache.cc
+++ lab/lock_server_cache.cc
@@ -91,9 +91,9 @@ lock_server_cache::revoker()
         //get the list mutex again
         pthread_mutex_lock(&revoke_list_mutex);
       }
-    }else{
-      pthread_cond_wait(&revoker_condition, &revoke_list_mutex); 
     }
+    pthread_cond_wait(&revoker_condition, &revoke_list_mutex); 
+    
   }
 
   pthread_mutex_unlock(&revoke_list_mutex);
@@ -127,9 +127,9 @@ lock_server_cache::retryer()
         //get the list mutex again
         pthread_mutex_lock(&retry_list_mutex);
       }
-    }else{
-      pthread_cond_wait(&retryer_condition, &retry_list_mutex); 
     }
+    pthread_cond_wait(&retryer_condition, &retry_list_mutex); 
+    
   }
 
   pthread_mutex_unlock(&retry_list_mutex);
